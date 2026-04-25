9effbb127be08caa95710f6accdd03177fd02de1
RDC1997 <rldd3d@gmail.com>
2026-04-25 14:13:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -85,6 +85,23 @@ with col2:
     )
     data = st.date_input("Data", datetime.today())
 
+if tipo == "Despesa":
+    categoria = st.selectbox(
+        "Categoria",
+        [
+            "Renda",
+            "Água",
+            "Luz",
+            "Vodafone",
+            "Alimentação",
+            "Gasolina",
+            "Outros"
+        ]
+    )
+
+    if categoria == "Outros":
+        descricao = st.text_input("Descrição")
+
 categoria = ""
 descricao = ""
 
